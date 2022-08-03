ddd72e42dedc47dc2a86908f5f151672a78b5a93
EhsanGheychisaz <ehsan.ghehisaz82@gmail.com>
2022-08-03 21:21:47 +0430

    add to useful feature for save the result of procedure

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
-import FIrstProject from './first/FIrstProject';
+import Calclus from './first/FIrstProject';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <FIrstProject />
+    <Calclus />
   </React.StrictMode>
 );
 
